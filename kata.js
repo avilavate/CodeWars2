ac4db6ce71ebe9732d70e121d20ceeaf6d48ac5a
A <avilavate@live.in>
2019-08-06 15:18:24 +0530

    Fixed AddInfinity Calc

--- kata.js
+++ kata.js
@@ -1,55 +1,55 @@
 /*jshint esversion: 6 */
 
 function AddInfinity(op1, op2) {
-    op1=op1.reverse();
-    op2=op2.reverse();
-    let hasil = 0, unitPlace;result="";
+    op1 = op1.reverse();
+    op2 = op2.reverse();
+    let hasil = 0, unitPlace; result = "";
     op1.forEach((element, index) => {
-        let temp = new Number(element) + new Number(op2[index]) +Number(hasil);
-    //    console.log(temp);
-        if(String(temp).length>1){
-            hasil=String(temp).split("")[0];
-            unitPlace=String(temp).split("")[1];
-            console.log(hasil + " " +unitPlace);
+        let temp = new Number(element) + new Number(op2[index]) + Number(hasil);
+
+        if (String(temp).length > 1) {
+            hasil = String(temp).split("")[0];
+            unitPlace = String(temp).split("")[1];
+
         }
-        else{
-            unitPlace=String(temp).split("")[0];
+        else {
+            unitPlace = String(temp).split("")[0];
         }
-        //console.log(unitPlace);
-        result+=unitPlace;
-        
+
+        result += unitPlace;
+
     });
-    console.log(result);
+    return String(result).split("").reverse().join("");
 }
-AddInfinity('10'.split(""),'19'.split(""));
+//console.log(AddInfinity('10'.split(""),'101'.split("")));
 function sumStrings(a, b) {
-    //console.log("Called");
     if (!isNaN(new Number(a)) && !isNaN(new Number(a))) {
         let aNum = a.split("");
         let bNum = b.split("");
-        //  console.log(aNum.length + " "+ bNum.length);
+
         let leading0 = aNum.length < bNum.length ? bNum.length - aNum.length : aNum.length - bNum.length;
         let leading0Str = '';
         for (let index = 0; index < leading0; index++) {
             leading0Str += "0";
         }
 
-        let newOpr = aNum.length < bNum.length ? () => {
-            let temp = new Number(leading0Str + a);
-            temp = String(temp).split("");
-            return AddInfinity(temp, b.split(""));
+        let result = aNum.length < bNum.length ? () => {
+            let temp = leading0Str + a;
+            return { 'a': temp, 'b': b };
         } :
             () => {
-                let temp = new Number(leading0Str + b);
-                return temp + new Number(a);
+                let temp = leading0Str + b;
+                return { 'a': a, 'b': temp };
             };
-        // console.log(leading0Str);
-        console.log(newOpr().toLocaleString());
 
+        //console.log(newOpr().toLocaleString());
+        return (result());
     }
 
     //return isNaN(new Number(a)) && isNaN(new Number(a))?null:new String( new Number(a)+ new Number (b));
 }
 
 
-//sumStrings('1268768768763', '1');
+let { a, b } = sumStrings('12687687687637645749854398543785439855645', '1');
+
+console.log(AddInfinity(String(a).split(""),String(b).split("")));
